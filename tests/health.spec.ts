bbd97d2c676051f7e00dd76990ea4dcc1d86c3cf
Rahima Irfan <rahimairfan.official@gmail.com>
2026-01-22 11:13:27 +0500

    added decorators

--- tests/health.spec.ts
+++ tests/health.spec.ts
@@ -2,7 +2,7 @@ import { BASE_URL, loginbuton } from "../utils/env";
 import { test, expect } from '@playwright/test';
 import { healthpage } from "../pages/healthpage";
 
-test.describe('Application health check', () => {
+test.describe('@smoke Application health check', () => {
     test('application loads', async ({ page }) => {
         const hp = new healthpage(page);
         await hp.navigate();
